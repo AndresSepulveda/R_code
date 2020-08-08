2c186e18a148e598c114f25bc9380d44ef01958e
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2020-08-07 23:47:35 -0400

    Find origin of particles that arrived, within a radius, of an AMERB

--- plot_OpenDrift_AMERS.R
+++ plot_OpenDrift_AMERS.R
@@ -33,11 +33,39 @@ lat_a <- amers[,2]
 tutto <- 0
 dista    <- c()
 mindista <- matrix(, nrow=length(5), ncol=length(lonend))
+ori <- c()
+lat_ori <- c()
+lon_ori <- c()
+all_lonlat_ori <- c()
+m <- 2
 for (i in 1:length(lon_a)) {
+    lonlat_ori <- c()
     dista <- geodist_vec(lon_a[i],lat_a[i],lonend,latend,measure="haversine")/1000
     indx <- which(dista < 1) # particulas que terminan a menos de 1 km de esa AMERB
-    auxtutto <- (length(indx))
+    if (length(indx) > 0){
+      iindx <- length(indx) + 1
+      lonlat_ori <- matrix(,nrow=iindx,ncol=2)
+      lonlat_ori[1,] <- cbind(lon_a[i],lat_a[i])
+      for (j in 2:iindx) {
+        jm1 <- j -1
+        aux <- indx[jm1]
+        #
+        #  De donde vienen, las que llegaron
+        #
+        lonlat_ori[j,] <- cbind(lonini[aux],latini[aux]) 
+      }
+      all_lonlat_ori[[m]] <- lonlat_ori
+      m <- m + 1
+    }  
+    auxtutto <- length(indx)
     tutto = tutto + auxtutto
 }
-print(tutto)
-print((tutto/length(lonend))*100)
+ctutto <- as.character(tutto)
+porcen <- (tutto/length(lonend))*100
+cporcen <- as.character(porcen)
+cat(ctutto," particulas llegaron a una AMERB, un ",cporcen, "%")
+
+#
+# TODO ¿Como exportamos la lista all_lonlat_ori?
+#
+
