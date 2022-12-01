421d964f95431e1be1bdaa0acbaf523e4af2e198
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2022-12-01 16:25:23 -0300

    Update World_Heatmap.R

--- World_Heatmap.R
+++ World_Heatmap.R
@@ -1,6 +1,8 @@
 #
 # https://stackoverflow.com/questions/61838700/query-on-how-to-make-world-heat-map-using-ggplot-in-r
 #
+# Desde la línea de comando: Rscript World_Heatmap.R
+#
 library(maps)
 library(ggplot2)
 
@@ -14,8 +16,8 @@ world_map <- subset(world_map, region != "Antarctica")
 ggplot(mydata) +
   geom_map(
     dat = world_map, map = world_map, aes(map_id = region),
-    fill = "white", color = "#7f7f7f", size = 0.25
+    fill = "white", color = "#7f7f7f", linewidth = 0.25
   ) +
-  geom_map(map = world_map, aes(map_id = Country, fill = Members), size = 0.25) +
+  geom_map(map = world_map, aes(map_id = Country, fill = Members), linewidth = 0.25) +
   scale_fill_gradient(low = "#fff7bc", high = "#cc4c02", name = "Participantes") +
   expand_limits(x = world_map$long, y = world_map$lat)
